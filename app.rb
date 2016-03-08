6dfacf6af7213f9f6332c83bcbe6a151cad006bd
Maxim Lakin <maximlakin@gmail.com>
2016-03-08 10:00:24 -0800

    cleanup

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require "bundler/setup"
 require "sinatra"
 require "feedjira"
+require 'json'
 
 get "/" do
   url = "https://www.reddit.com/r/news/.rss"
   feed = Feedjira::Feed.fetch_and_parse url
+  # content_type :json
+
+  
   erb :index, :layout => :main, :locals => {:feed => feed}
 end
